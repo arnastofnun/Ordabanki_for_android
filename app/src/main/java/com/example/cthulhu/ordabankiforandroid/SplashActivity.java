f74c9881a10f0535afe345867fa57efc73a13456
karlasgeir <karlasgeir@gmail.com>
2014-09-29 23:00:49 +0000

    Created the start screen and select language screen

--- app/src/main/java/com/example/cthulhu/ordabankiforandroid/SplashActivity.java
+++ app/src/main/java/com/example/cthulhu/ordabankiforandroid/SplashActivity.java
@@ -1,24 +1,39 @@
 package com.example.cthulhu.ordabankiforandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class SearchScreen extends Activity {
+public class SplashActivity extends Activity {
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_screen);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(SplashActivity.this,SelectLanguageActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+                /*Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();*/
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.search_screen, menu);
+        getMenuInflater().inflate(R.menu.splash, menu);
         return true;
     }
 
