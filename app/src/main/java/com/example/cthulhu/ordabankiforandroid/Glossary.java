8d36fae7ea003bce1078f91d324648062f4c4f4b
karlasgeir <karlasgeir@gmail.com>
2014-10-14 23:52:43 +0000

    Including the select_language.xml file

--- app/src/main/java/com/example/cthulhu/ordabankiforandroid/Glossary.java
+++ app/src/main/java/com/example/cthulhu/ordabankiforandroid/Glossary.java
@@ -2,6 +2,7 @@ package com.example.cthulhu.ordabankiforandroid;
 
 /**
  * Created by karlasgeir on 14.10.2014.
+ * This class contains the glossary object
  */
 public class Glossary{
 
@@ -9,6 +10,7 @@ public class Glossary{
     String name = null;
     boolean selected = false;
 
+    //Creates the glossary object
     public Glossary(String code, String name, boolean selected) {
         super();
         this.code = code;
@@ -16,22 +18,32 @@ public class Glossary{
         this.selected = selected;
     }
 
+    //Returns the code for the glossary
     public String getCode() {
         return code;
     }
+
+    //Sets the code for the glossary
     public void setCode(String code) {
         this.code = code;
     }
+
+    //Returns the name of the glossary
     public String getName() {
         return name;
     }
+
+    //Sets the name of the glossary
     public void setName(String name) {
         this.name = name;
     }
 
+    //Returns true if glossary is selected, else false
     public boolean isSelected() {
         return selected;
     }
+
+    //Sets glossary selected status
     public void setSelected(boolean selected) {
         this.selected = selected;
     }
