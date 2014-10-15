fe3869703f089bbacbbe49b14431388c0c035d99
karlasgeir <karlasgeir@gmail.com>
2014-10-15 22:36:34 +0000

    .

--- app/src/main/java/com/example/cthulhu/ordabankiforandroid/SearchScreenFragment.java
+++ app/src/main/java/com/example/cthulhu/ordabankiforandroid/SearchScreenFragment.java
@@ -1,13 +1,11 @@
 package com.example.cthulhu.ordabankiforandroid;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -43,7 +41,7 @@ public class SearchScreenFragment extends Fragment {
         //Add items to target language dropdown spinner
         targetSpinner = (Spinner) rootView.findViewById(R.id.targetSpinner);
         List<String> listTarget = new ArrayList<String>();
-        listTarget.add("All");
+        listTarget.add(getResources().getString(R.string.all_languages));
         listTarget.add("example");
         listTarget.add("spinner");
 
