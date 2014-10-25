33f73c1ece9f8188e906e1b89ef906ec82d3a919
Trausti Kouichi <traustiag@gmail.com>
2014-10-25 00:56:05 +0000

    Switched to Robotium for testing and it works great, see SearchScreenTest for a simple example. I also deleted old tests

--- app/src/androidTest/java/com/example/cthulhu/ordabankiforandroid/unittests/SearchScreenTest.java
+++ app/src/androidTest/java/com/example/cthulhu/ordabankiforandroid/unittests/SearchScreenTest.java
@@ -1,14 +1,11 @@
 package com.example.cthulhu.ordabankiforandroid.unittests;
 
-import android.support.v4.view.ViewPager;
 import android.test.ActivityInstrumentationTestCase2;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.example.cthulhu.ordabankiforandroid.ChooseLanguagesFragment;
 import com.example.cthulhu.ordabankiforandroid.R;
 import com.example.cthulhu.ordabankiforandroid.SearchScreen;
+import com.robotium.solo.Solo;
 
 /**
  * @author Trausti
@@ -16,27 +13,52 @@ import com.example.cthulhu.ordabankiforandroid.SearchScreen;
  */
 public class SearchScreenTest extends ActivityInstrumentationTestCase2<SearchScreen> {
 
-    //instance variable
+    private Solo solo;
     private SearchScreen activity;
 
     public SearchScreenTest() {
         super(SearchScreen.class);
     }
 
-    @Override
-    protected void setUp() throws Exception{
-        super.setUp();
-        activity = getActivity();
+    public void setUp() throws Exception {
+        solo = new Solo(getInstrumentation(), getActivity());
+
     }
 
-    public void testPreConditions() {
-    } // end of testPreConditions() method definition
-
-    public void testStateDestroy() {
+    //Are we testing the correct activity?
+    public void testPreconditions(){
+        solo.assertCurrentActivity("wrong activity",SearchScreen.class);
     }
+
+    /**
+     * This method tests if spinner values stay the same after
+     * having exited and revisited the activity that contains them
+     * Written by Trausti
+     * @return      nothing
+     */
+    public void testSpinner(){
+        //Click on Languages tab
+        solo.clickOnText(solo.getString(R.string.languages_tab_name));
+        //Press spinner item 0 (src lang) and language 2 (English)
+        solo.pressSpinnerItem(0,2);
+        //Press spinner item 1 (trg lang) and language 1 (Icelandic)
+        solo.pressSpinnerItem(1,1);
+        //Click on search tab
+        solo.clickOnText(solo.getString(R.string.search_tab));
+        //Click on Languages tab again
+        solo.clickOnText(solo.getString(R.string.languages_tab_name));
+        //check if spinner items still have the same values
+        assertTrue(solo.isSpinnerTextSelected("English"));//spinner item 0
+        assertTrue(solo.isSpinnerTextSelected("Icelandic"));//spinner item 1
+    }
+
+
+
+
+
     @Override
-    protected void tearDown() throws Exception{
-        super.tearDown();
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
     }
 
 }
